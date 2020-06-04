1906912c0af5613ae2fb4b0c6e01c50d2436df33
KOSHVENDRA <57467409+KOSHVENDRA@users.noreply.github.com>
2020-06-04 16:41:52 +0000

    factors in dft has been multiplied

--- FT_const.py
+++ FT_const.py
@@ -7,14 +7,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 const=float(input('Enter c for constant function f(x)=c\n'))
-No=int(input('enter no. of sample points : No\n'))
+#No=int(input('enter no. of sample points : No\n'))
 
-inp_arr=np.ones(No)*const   #  input array to be fourier transformed
+num=256
+inp_arr=np.ones(num)*const   #  input array to be fourier transformed
+xmin=-20
+xmax=20
+dx=(xmax-xmin)/(num-1)
 
-dft=np.sqrt(No/(2*np.pi)) *(np.fft.fft(inp_arr,norm='ortho'))    # calculation of dft
-dft_shift=np.fft.fftshift(dft)
-
-freq= 2*np.pi*np.fft.fftfreq(No)                # frequency sample
+dft=np.sqrt(num/(2*np.pi)) *(np.fft.fft(inp_arr,norm='ortho'))    # calculation of dft
+freq= 2*np.pi*np.fft.fftfreq(num)                # frequency sample
+factor=np.exp(-1j*freq*xmin)   
+DFT=dx*factor*dft
+dft_shift =np.fft.fftshift(DFT) 
 freq_shift=np.fft.fftshift(freq)
 
 plt.plot(freq_shift,dft_shift)
